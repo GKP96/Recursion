fc0f40ed4f366bdd04a36e64eff24b8d4da5f7e6
Gautam <gautam2002pandeyjee@email.com>
2022-11-12 09:59:51 +0530

    third-Update

---
+++ PallindromeCheck.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class PallindromeCheck {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        if(isPalindrome(str)){
+            System.out.println("Is pallindrome");
+        }else{
+            System.out.println("Not pallindrome");
+        }
+
+    }
+    static boolean isPalindrome(String str){
+        if(str.equals(""))return true;
+        if(str.length()==1)return true;
+        return (str.charAt(0)==str.charAt(str.length()-1))&&isPalindrome(str.substring(1,str.length()-2));
+    }
+}
