1cfcf58349efbed63c1c390527e65fbece0bc5d3
Gautam <gautam2002pandeyjee@email.com>
2022-11-12 09:36:50 +0530

    Second-Update

--- StringReversal.java
+++ StringReversal.java
@@ -1,4 +1,19 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class StringReversal {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        String newStr = reverseString(str);
+        System.out.println(newStr);
+
+    }
+    // Yha kya ho rha hai.
+
+    static String reverseString(String str){
+        if(str.equals(""))return "";
+       return reverseString(str.substring(1))+str.charAt(0);
+    }
 }
